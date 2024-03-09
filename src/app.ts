138c4890c1c2c26e1966616272d21030c5d7b650
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-03-09 08:14:05 -0500

    feat: :fire: finished envs and routes initial setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hola mundo');
+import { Server } from "./presentation/server";
+import { envs } from './config/envs';
+import { AppRoutes } from "./presentation/routes";
+
+(async()=> {
+  main();
+})();
+
+
+async function main() {
+
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
