ec1e126e7d93c7c68b2d3a56a8c72184d376ee0c
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-03-10 21:07:02 -0500

    fix: :wrench: updated evironments

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,7 +5,6 @@ import { get } from 'env-var';
 export const envs = {
 
   PORT: get('PORT').required().asPortNumber(),
-  PUBLIC_URL: get('PUBLIC_URL').required().asString(),
   JWT_SEED: get('JWT_SEED').required().asString(),
 
 }
